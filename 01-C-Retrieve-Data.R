92c23803c54443171e2e26d09d771fccf9654fb8
Einzbern <ylwpaopao@163.com>
2018-06-29 22:48:55 +0800

    First commit.

---
+++ 01-C-Retrieve-Data.R
@@ -0,0 +1,10 @@
+# Copyright Reserved
+
+#####----Guidance----#####
+
+library(tidyverse)
+library(lubridate)
+
+
+#####----读取数据----#####
+
---
+++ 03-C-Weekly.R
@@ -0,0 +1,11 @@
+# Copyright Reserved
+
+#####----Guidance----#####
+
+library(tidyverse)
+library(lubridate)
+library(quantmod)
+library(DT)
+
+
+#####----读取数据----#####
