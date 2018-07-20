be77decc7ad1774a26eda6ab7ff2b7da5558d72f
Einzbern <ylwpaopao@163.com>
2018-07-20 13:04:04 +0800

    Original ui.

--- global.R
+++ global.R
@@ -1,2 +1,11 @@
 library(shiny)
 library(tidyverse)
+library(lubridate)
+library(DT)
+
+load("./data/ISI-Daily.RData")
+
+ISI_Daily <- ISI_Daily %>% 
+  mutate(TRADE_DT = ymd(TRADE_DT))
+
+start_date <- ISI_Daily[1, "TRADE_DT", drop = T]
